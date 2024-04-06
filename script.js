438f4a5d1de1ae36e9ba797482eca43ebcd14baf
kom483 <preetkoaml1130@gmail.com>
2024-04-06 15:53:44 +0530

    add new map in js

---
+++ script.js
@@ -0,0 +1,8 @@
+//map = Some operation,the value its callback returns are used to form new array.
+let nums = [67,52,39];
+nums.map((val)=>{
+    console.log(val);
+});
+let calSquare = (num)=>{
+    console.log(num*num);
+} 
